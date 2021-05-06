5fecd448f53bba6e5788a232cef2bac1fd0beacc
ajdaw2020 <83763109+ajdaw2020@users.noreply.github.com>
2021-05-06 13:53:48 +0200

    Add files via upload

---
+++ practica-ajbllo.php
@@ -0,0 +1,67 @@
+ <?php
+ /**
+******************************************************
+*  @file test.php
+*  @brief Breve descripción
+* Pequeña documentación del archivo
+* @author Gaspar
+* @version 2.0
+* @date Marzo 2010
+*
+*
+*******************************************************/
+
+/**
+* Variable global
+*/
+$var_global='VALOR';
+
+/** Ahora me da por definir cosas */
+define('defino_algo', 'DEFINICION');
+/*
+* Description: Esta función algo hará
+*
+* @param $cadena
+* Esto es una cadena graciosa
+* @param $vector
+* Esto es un vestor gracioso
+*
+* @return string
+* Cadena resultante
+
+*/
+function estoyprobando($cadena, $vector)
+{
+}
+
+/**
+******************************************************
+* @brief Clase para hacer pruebas
+*
+******************************************************/
+class TestingPHP
+{
+/**
+*************************************************************
+* @brief Constructor
+*************************************************************/
+function __construct()
+{
+}
+
+/**
+*************************************************************
+* @brief Metodo 1
+* @param $par Primer parametro de entrada
+*
+* @return Genero una salida
+*************************************************************/
+function metodo1($par)
+{
+/**
+*************************************************************
+@internal esta infromación solo será de disposición para los desarrolladores
+*************************************************************/ 
+}
+};
+?>; 
